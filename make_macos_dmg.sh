e465ed1f74d3341bbfc1872e51b7d69bba865940
Bjoern Kerler <info@revskills.de>
2026-04-18 18:44:45 +0200

    Update mac signing

--- make_macos_dmg.sh
+++ make_macos_dmg.sh
@@ -9,4 +9,4 @@ rm -rf dist/
 rm -rf logs/
 python3 -m briefcase create macOS Xcode
 python3 -m briefcase build macOS Xcode
-python3 -m briefcase package macOS Xcode
+python3 -m briefcase package macOS Xcode -i 0FACCD5E48260DDA00C85D517BCAD13F022A28F1
